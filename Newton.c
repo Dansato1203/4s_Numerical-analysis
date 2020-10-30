e2d92146d3a36db035ab93ab56862e39fe78cde2
Dan Sato <s19C1054BU@s.chibakoudai.jp>
2020-10-30 17:28:17 +0900

    Add a file

---
+++ Newton.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double f(double x){
+  return cos(x)+x;
+}
+
+double d_f(double x){
+  return -sin(x)+1;
+}
+
+int main(){
+  int  n_max;
+  int n = 0;
+  double x, eps, del;
+
+  printf("初期値x,小さい数eps,最大繰り返し回数n_maxを入力\n");
+  printf("x = ");
+  scanf("%lf", &x);
+  printf("eps = ");
+  scanf("%lf", &eps);
+  printf("n_max = ");
+  scanf("%d", &n_max);
+
+  do {
+    del = -f(x) / d_f(x);
+    x = x + del;
+    n++;
+    printf("[round: %d], x = %.10lf\n", n, x);
+  }while(fabs(del) > eps && n <= n_max);
+
+  if(n == n_max){
+    printf("失敗。解が見つからない\n");
+  }else{
+    printf("近似解は%.10lf\n試行回数は%d回\n", x, n);
+  }
+
+  return 0;
+}
