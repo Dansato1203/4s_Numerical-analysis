2ec6083e7c0a68cf69583f7fa15f22d8483ce834
Dan Sato <s19C1054BU@s.chibakoudai.jp>
2020-11-23 23:43:24 +0900

    Add a file

---
+++ daikei.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+double f (double x){
+  return x*x*x + 3*x*x + x + 1;
+}
+
+double daikei(double a, double b, int n){
+  double delta = (b - a) / n;
+  double x, value;
+  int i;
+  for(int i=0; i<n; i++) {
+    
+    // i番目の台形の左下X値
+    x = a + i * delta;
+    
+    // 積分値に i 番目の台形の面積を足す
+    value += ( f(x) + f(x+delta) ) * delta / 2.0;
+}
+  printf("%lf\n", value);
+  return 0;
+}
+
+int main(){
+   double a, b;
+   int n;
+   printf("区間[a,b]を入力\n");
+   printf("a : ");
+   scanf("%lf", &a);
+   printf("b : ");
+   scanf("%lf", &b);
+   printf("分割数nを入力\n");
+   printf("n : ");
+   scanf("%d", &n);
+   daikei(a, b, n);
+   return 0;
+ }         
---
+++ simpson.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+
+double f(double x){
+  return x*x*x + 3*x*x + x + 1;
+}
+
+double simpson(double a, double b, int n){
+  double delta = (b - a) / n;
+  double x, y;
+  x = f(a);
+  x += f(a + delta * n);
+  int i;
+  for(i=1; i<n; i+=2){
+      x += 4.0 * f(a + delta * i);
+  }
+  for(i=2; i<n; i+=2){
+      x += 2.0 * f(a + delta * i);
+  }
+
+  y = x*delta/3.0;
+  printf("%lf\n", y);
+  return 0;
+}
+
+int main(){
+  double a, b;
+  int n;
+  printf("区間[a,b]を入力\n");
+  printf("a : ");
+  scanf("%lf", &a);
+  printf("b : ");
+  scanf("%lf", &b);
+  printf("分割数nを入力\n");
+  printf("n : ");
+  scanf("%d", &n);
+  simpson(a, b, n);
+  return 0;
+}
